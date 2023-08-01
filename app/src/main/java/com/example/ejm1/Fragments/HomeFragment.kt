6e6eb50c8685cad0378ac3bbe2da47871a17a4e0
aaronrc23 <aaronrc2022@gmail.com>
2023-08-01 16:40:04 -0500

    actualizacionv1

--- app/src/main/java/com/example/ejm1/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/ejm1/Fragments/HomeFragment.kt
@@ -5,29 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+
 import com.denzcoskun.imageslider.ImageSlider
 import com.denzcoskun.imageslider.models.SlideModel
 import com.example.ejm1.R
+import androidx.recyclerview.widget.RecyclerView
+
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.ejm1.Adapter.ProductoAdapter
+import com.example.ejm1.Clases.Product
+import com.example.ejm1.Object.RetrofitClient
+
+
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class HomeFragment : Fragment() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var miAdapter: ProductoAdapter
+    private val productList = mutableListOf<Product>()
+
     // Crea la lista de imágenes
     private val imageList = ArrayList<SlideModel>()
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        // Inflar el layout del fragmento
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        // Obtener la referencia al RecyclerView
+        recyclerView = view.findViewById(R.id.rcvcardprod)
+
+        // Configurar el RecyclerView con un LinearLayoutManager
+        val layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.layoutManager = layoutManager
+
+        // Crear e inicializar el adaptador
+        miAdapter = ProductoAdapter(productList)
+        recyclerView.adapter = miAdapter
+
+        return view
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -42,6 +66,36 @@ class HomeFragment : Fragment() {
 
         // Establece la lista de imágenes en el control de deslizamiento de imágenes
         imageSlider.setImageList(imageList)
+
+        // Llamar al método para obtener los productos de la API
+        obtenerProductos()
     }
+
+    private fun obtenerProductos() {
+        val retrofitTraer = RetrofitClient.consumirApi.getTraer()
+
+        retrofitTraer.enqueue(object : Callback<List<Product>> {
+            override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
+                if (response.isSuccessful) {
+                    val productos = response.body()
+                    if (productos != null) {
+                        miAdapter.setData(productos) // Llenar el adaptador con los productos obtenidos
+                    } else {
+                        Toast.makeText(activity, "No se encontraron productos.", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(activity, "Error al obtener los productos.", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onFailure(call: Call<List<Product>>, t: Throwable) {
+                Toast.makeText(activity, "Error al consultar API Rest", Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
+
+
+
 
 }
--- app/src/main/java/com/example/ejm1/Fragments/PerfilFragment.kt
+++ app/src/main/java/com/example/ejm1/Fragments/PerfilFragment.kt
@@ -12,13 +12,9 @@ import com.example.ejm1.R
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [PerfilFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class PerfilFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+
     private var param1: String? = null
     private var param2: String? = null
 
@@ -39,14 +35,7 @@ class PerfilFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PerfilFragment.
-         */
+
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
